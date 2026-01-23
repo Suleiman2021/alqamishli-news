8cef5bef41efc5770bac2406f55b324c36a8575f
Suleiman2021 <nsuleimanalothman@gmail.com>
2026-01-23 04:22:04 +0300

    Configure production environment

--- app/config.py
+++ app/config.py
@@ -1,16 +1,15 @@
-# app/config.py
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev-key")
+
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
 
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = ('slslkennews', os.getenv('MAIL_USERNAME'))
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = ('slslkennews', os.getenv("MAIL_USERNAME"))
