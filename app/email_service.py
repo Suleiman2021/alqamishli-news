941f2ff4c91b9c5514bfeaf9d0f64487f1718f2d
Suleiman2021 <nsuleimanalothman@gmail.com>
2026-01-26 01:54:22 +0300

    Replace

--- app/email_service.py
+++ app/email_service.py
@@ -5,7 +5,7 @@ resend.api_key = os.getenv("RESEND_API_KEY")
 
 def send_email(to, subject, html):
     resend.Emails.send({
-        "from": "Al-Qamishli News <onboarding@resend.dev>",
+        "from": "Al-Qamishli News <no-reply@alqamishli-news.onrender.com>",
         "to": [to],
         "subject": subject,
         "html": html
